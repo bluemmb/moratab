2eea4c79ec17acb3c904066ee5cbf8798f0895a4
Mohammad Eftekhari <bluemmb22@gmail.com>
2017-09-19 19:02:44 +0430

    Use pagedown-extra libraries for View
    The editor is using pagedown-extra libraries in it's underhood.
    So using same library scripts for View is more sensible.
    
    The Markdown.Editor.js file was modified and it was actually
    named buttons.js in this project.
    
    - Files are downloaded from this repository:
    	https://github.com/jmcmanus/pagedown-extra
    
    - Commit SHA:
    	a5d60596fc9cd9f73efe2f5d955ed1ceab642fb5

--- editor/buttons.js
+++ editor/buttons.js
@@ -1,43 +1,5 @@
-var Markdown = {};
 
-(function () {
-
-	function identity(x) { return x; }
-	function returnFalse(x) { return false; }
-
-	function HookCollection() { }
-
-	HookCollection.prototype = {
-
-		chain: function (hookname, func) {
-			var original = this[hookname];
-			if (!original)
-				throw new Error("unknown hook " + hookname);
-
-			if (original === identity)
-				this[hookname] = func;
-			else
-				this[hookname] = function (text) {
-					var args = Array.prototype.slice.call(arguments, 0);
-					args[0] = original.apply(null, args);
-					return func.apply(null, args);
-				};
-		},
-		set: function (hookname, func) {
-			if (!this[hookname])
-				throw new Error("unknown hook " + hookname);
-			this[hookname] = func;
-		},
-		addNoop: function (hookname) {
-			this[hookname] = identity;
-		},
-		addFalse: function (hookname) {
-			this[hookname] = returnFalse;
-		}
-	};
-
-	Markdown.HookCollection = HookCollection;
-})();
+// This is Modified Markdown.Editor.js
 
 (function () {
 
